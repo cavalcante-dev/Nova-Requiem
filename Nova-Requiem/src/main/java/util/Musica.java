2a2ceae8798acec7deeab5fd77f74941a6ebd351
miguelalchaar <alchaarcs@gmail.com>
2025-06-14 15:53:14 -0300

    teste música

--- Nova-Requiem/src/main/java/util/Musica.java
+++ Nova-Requiem/src/main/java/util/Musica.java
@@ -11,7 +11,11 @@ public class Musica {
     public void trilhaSonora(int capitulo) {
         if (capitulo == 1) {
             try {
+<<<<<<< HEAD
                 musica = AudioSystem.getAudioInputStream(new File("Nova-Requiem\\Nova-Requiem\\src\\main\\java\\util\\musicas\\Instrument-of-Surrender-Sea-Power.wav").getAbsoluteFile());
+=======
+                musica = AudioSystem.getAudioInputStream(new File("src/main/java/util/musicas/Instrument-of-Surrender-Sea-Power.wav").getAbsoluteFile());
+>>>>>>> 59c7deda3ed599e7354a52334a796f53cb0b9d58
                 clip = AudioSystem.getClip();
                 clip.open(musica);
                 clip.start();
@@ -23,7 +27,11 @@ public class Musica {
         }
         if (capitulo == 2) {
             try {
+<<<<<<< HEAD
                 musica = AudioSystem.getAudioInputStream(new File("Nova-Requiem\\Nova-Requiem\\src\\main\\java\\util\\musicas\\Ecstatic-Vibrations_-Totally-Transcendent-Sea-Power (1).wav").getAbsoluteFile());
+=======
+                musica = AudioSystem.getAudioInputStream(new File("src/main/java/util/musicas/Ecstatic-Vibrations_-Totally-Transcendent-Sea-Power (1).wav").getAbsoluteFile());
+>>>>>>> 59c7deda3ed599e7354a52334a796f53cb0b9d58
                 clip = AudioSystem.getClip();
                 clip.open(musica);
                 clip.start();
@@ -35,7 +43,11 @@ public class Musica {
         }
         if (capitulo == 3) {
             try {
+<<<<<<< HEAD
                 musica = AudioSystem.getAudioInputStream(new File("Nova-Requiem\\Nova-Requiem\\src\\main\\java\\util\\musicas\\Whirling-In-Rags-12-PM-Sea-Power (2).wav").getAbsoluteFile());
+=======
+                musica = AudioSystem.getAudioInputStream(new File("src/main/java/util/musicas/Whirling-In-Rags-12-PM-Sea-Power (2).wav").getAbsoluteFile());
+>>>>>>> 59c7deda3ed599e7354a52334a796f53cb0b9d58
                 clip = AudioSystem.getClip();
                 clip.open(musica);
                 clip.start();
@@ -49,7 +61,11 @@ public class Musica {
 
     public void rolarDado() {
         try {
+<<<<<<< HEAD
             musica = AudioSystem.getAudioInputStream(new File("Nova-Requiem\\Nova-Requiem\\src\\main\\java\\util\\musicas\\Dice-Roll-Sound.wav").getAbsoluteFile());
+=======
+            musica = AudioSystem.getAudioInputStream(new File("src/main/java/util/musicas/Dice-Roll-Sound.wav").getAbsoluteFile());
+>>>>>>> 59c7deda3ed599e7354a52334a796f53cb0b9d58
             clip = AudioSystem.getClip();
             clip.open(musica);
             clip.setFramePosition(1800);
